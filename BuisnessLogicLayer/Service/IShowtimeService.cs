1821e0bf358d056625bff3385948e483231459a3
MANSY <MANSY@DESKTOP-AGHBKJS>
2026-09-16 00:42:48 +0200

    feat: complete showtimes and seats backend implementation

---
+++ BuisnessLogicLayer/Service/IShowtimeService.cs
@@ -0,0 +1,12 @@
+﻿using CinemaBooking.Models;
+
+namespace BuisnessLogicLayer.Service
+{
+    public interface IShowtimeService
+    {
+        Task<IEnumerable<Showtime>> GetShowtimesByMovieIdAsync(int movieId);
+        Task<Showtime?> GetShowtimeByIdAsync(int showtimeId);
+        Task<IEnumerable<Seat>> GetSeatsByHallIdAsync(int hallId);
+        Task<IEnumerable<int>> GetBookedSeatIdsAsync(int showtimeId);
+    }
+}
---
+++ CinemaBooking/Controllers/ShowtimesController.cs
@@ -0,0 +1,32 @@
+﻿using BuisnessLogicLayer.Service;
+using Microsoft.AspNetCore.Mvc;
+using PresentationLayer.ViewModels;
+
+namespace PresentationLayer.Controllers
+{
+    public class ShowtimesController : Controller
+    {
+        private readonly IShowtimeService _showtimeService;
+
+        public ShowtimesController(IShowtimeService showtimeService)
+        {
+            _showtimeService = showtimeService;
+        }
+
+        public async Task<IActionResult> Index(int movieId)
+        {
+            var showtimes = await _showtimeService.GetShowtimesByMovieIdAsync(movieId);
+
+            var viewModelList = showtimes.Select(s => new ShowtimeViewModel
+            {
+                ShowtimeId = s.Id,
+                MovieId = s.MovieId,
+                MovieTitle = s.Movie?.Title ?? "",
+                CinemaName = s.Hall?.Cinema?.Name ?? "",
+                HallName = s.Hall?.Name ?? ""
+            }).ToList();
+
+            return View(viewModelList);
+        }
+    }
+}
