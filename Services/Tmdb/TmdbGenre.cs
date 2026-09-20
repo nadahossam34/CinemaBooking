7094622cf0814779b915efa13f43d6199f9a283f
nadahossam34 <nh941933@gmail.com>
2026-09-21 00:19:41 +0300

    Add files via upload

---
+++ Services/Tmdb/TmdbGenre.cs
@@ -0,0 +1,13 @@
+using System.Text.Json.Serialization;
+
+namespace CinemaBooking.Services.Tmdb
+{
+    public class TmdbGenre
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+    }
+}
