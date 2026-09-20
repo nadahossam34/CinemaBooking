3db5d84d96bd357e7adb75ac42e9da83c4b2699e
nadahossam34 <nh941933@gmail.com>
2026-09-21 00:20:26 +0300

    Add files via upload

---
+++ Controllers/CinemasController.cs
@@ -0,0 +1,46 @@
+﻿using BuisnessLogicLayer.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly ICinemaService _cinemaService;
+
+        public CinemasController(ICinemaService cinemaService)
+        {
+            _cinemaService = cinemaService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var cinemas = await _cinemaService.GetAllCinemasAsync();
+
+            return View(cinemas);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var cinema = await _cinemaService.GetCinemaByIdAsync(id);
+
+            if (cinema == null)
+                return NotFound();
+
+            return View(cinema);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Nearest(
+            double latitude,
+            double longitude)
+        {
+            var cinema = await _cinemaService
+                .GetNearestCinemaAsync(latitude, longitude);
+
+            if (cinema == null)
+                return NotFound();
+
+            return View(cinema);
+        }
+    }
+}
